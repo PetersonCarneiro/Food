2c52b37ae537900795a447eb02c337a15f397c6c
Peterson <peters.prestes@gmail.com>
2024-04-23 09:37:55 -0300

    Feat: update ResponseEntity

--- src/main/java/br/controller/FoodController.java
+++ src/main/java/br/controller/FoodController.java
@@ -5,6 +5,7 @@ import br.domain.dto.FoodResponseDTO;
 import br.domain.entities.Food;
 import br.repository.FoodRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,17 +16,34 @@ import java.util.List;
 public class FoodController {
     @Autowired
     private FoodRepository foodRepository;
+
     @PostMapping
-    public void salveAll(@RequestBody FoodRequestDTO foodRequestDTO){
+    public ResponseEntity postFood(@RequestBody FoodRequestDTO foodRequestDTO){
+         Food foodDate = new Food(foodRequestDTO);
+         foodRepository.save(foodDate);
+         return  ResponseEntity.ok().build();
+    }
+    /*
+    @PostMapping
+    public void saveAll(@RequestBody FoodRequestDTO foodRequestDTO){
         Food foodDate = new Food(foodRequestDTO);
         foodRepository.save(foodDate);
     }
+     */
 
+    @GetMapping
+    public ResponseEntity getALL(){
+        List<FoodResponseDTO> foodList = foodRepository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return ResponseEntity.ok(foodList);
+    }
+
+    /*
     @GetMapping
     public List<FoodResponseDTO> getAll(){
         List<FoodResponseDTO> foodList = foodRepository.findAll().stream().map(FoodResponseDTO::new).toList();
         return foodList;
     }
+     */
 
 
 }
